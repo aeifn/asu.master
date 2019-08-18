7f994f1080752f14088ff073952ba692f9488ebc
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2019-08-18 15:59:06 +0300

    inc

--- include/tex.mk
+++ include/tex.mk
@@ -2,13 +2,16 @@ LATEX=pdflatex
 BIBTEX=biber
 # https://www.gnu.org/software/make/manual/html_node/Phony-Targets.html
 .PHONY: clean
+.PHONY: build
 
 all: $(TARGET).pdf
 
 clean:
 	rm -f *.aux *.log *.blg *~ *.bcf *.bbl *.toc *.out *.run.xml
 
-$(TARGET).pdf: $(TARGET).tex $(TARGET).bib ${INCLUDE}/common.tex
+$(TARGET).pdf: $(TARGET).tex $(TARGET).bib ${INCLUDE}/common.tex build
+
+build:
 	$(LATEX) $(TARGET)
 	$(BIBTEX) $(TARGET)
 	$(LATEX) $(TARGET)
